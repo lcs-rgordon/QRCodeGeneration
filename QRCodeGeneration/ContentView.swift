5e487872c3e50b7ec7465699e9564e141ac41106
Russell Gordon <rgordon@lcs.on.ca>
2024-03-06 09:29:14 -0500

    Initial Commit

---
+++ QRCodeGeneration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRCodeGeneration
+//
+//  Created by Russell Gordon on 2024-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRCodeGeneration/QRCodeGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGenerationApp.swift
+//  QRCodeGeneration
+//
+//  Created by Russell Gordon on 2024-03-06.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
